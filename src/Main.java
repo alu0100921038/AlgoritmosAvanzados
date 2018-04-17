9d3669b3d00dd63f522be88076137fbda45f4d0d
alu0100921038 <alu0100921038@ull.edu.es>
2018-04-17 14:04:12 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package practica10;
+
+import javax.swing.SwingUtilities;
+
+public class Main{
+	public static void main(String[] args) {
+		    SwingUtilities.invokeLater(new Runnable() {
+		        @Override
+		        public void run() {
+		            new modificacion(); 
+		        }
+		    });
+		}
+	}
+
